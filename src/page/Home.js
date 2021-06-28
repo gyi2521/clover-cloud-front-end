977b82d04711bc6e0cc53f1498404090e0738e86
nhyi <ginay2521@gmail.com>
2021-06-28 15:22:47 -0400

    updated image to fit the screen

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,14 +1,9 @@
 import React from "react";
-//import "../Home.css";
+import "../Home.css";
 import BgImg from "../images/TechTablanding.jpg";
-//import "./App.css";
 
 const Home = () => {
-  return (
-    <div className="page-heading">
-      <img src={BgImg} className="App-logo-sm" alt="logo" />
-    </div>
-  );
+  return <div className="homeBackground"></div>;
 };
 
 export default Home;
